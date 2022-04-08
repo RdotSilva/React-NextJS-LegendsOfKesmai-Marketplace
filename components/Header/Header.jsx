aca951349d2b9942d70ca18839fd4e10eb7c2ed6
RdotSilva <RyandotSilva@gmail.com>
2022-04-08 20:44:56 +0100

    Fix dragon logo

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <a href="#" className="flex items-center">
             <img
-              src="public\images\kesmai-dragon-red.png"
+              src="\images\kesmai-dragon-red.png"
               className="mr-3 h-6 sm:h-9"
               alt="Reg Dragon"
             ></img>
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              LOK Marketplace
             </span>
           </a>
           <button
