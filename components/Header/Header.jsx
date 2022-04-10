ec18ce7af37ef2e38c3db139c1eff5aec6738ab3
RdotSilva <RyandotSilva@gmail.com>
2022-04-10 20:07:48 +0100

    Update nav links

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                   aria-current="page"
                 >
@@ -61,7 +61,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="buy"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Buy
@@ -69,7 +69,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="sell"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Sell
@@ -77,7 +77,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="trade"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Trade
