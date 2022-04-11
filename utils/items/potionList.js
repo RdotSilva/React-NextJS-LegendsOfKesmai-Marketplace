66fc9ea20f3377eafcae945569d5fc7f0152a4f1
RdotSilva <RyandotSilva@gmail.com>
2022-04-11 19:43:10 +0100

    Add potionList constant

---
+++ utils/items/potionList.js
@@ -0,0 +1,21 @@
+export const potionList = {
+  strength: {
+    name: "Strength Potion",
+    image: "images/strength-potion.png",
+  },
+  dexterity: {
+    name: "Dexterity Potion",
+  },
+  intelligence: {
+    name: "Intelligence Potion",
+  },
+  wisdom: {
+    name: "Wisdom Potion",
+  },
+  willpower: {
+    name: "Willpower Potion",
+  },
+  constitution: {
+    name: "Constitution Potion",
+  },
+};
