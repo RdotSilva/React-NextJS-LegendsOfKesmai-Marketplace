c2c4c89034e23903d539484670fc37e2b96808a3
RdotSilva <RyandotSilva@gmail.com>
2022-04-12 18:53:26 +0100

    Add page for dynamic buying id

---
+++ pages/buying/[id].js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const BuyingDetails = () => {
+  return (
+    <>
+      <Layout />
+      <div>Buying Details Page</div>
+    </>
+  );
+};
+
+export default BuyingDetails;
