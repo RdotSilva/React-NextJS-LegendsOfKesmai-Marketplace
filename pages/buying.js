6293ee7ab22f108d73934e23435f40a9485f62f3
RdotSilva <RyandotSilva@gmail.com>
2022-04-12 18:37:16 +0100

    Add page for buying

---
+++ pages/buying.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Buying from "../components/Buy/Buying.jsx";
+import Layout from "../components/Layout/Layout.jsx";
+
+function BuyingPage() {
+  return (
+    <>
+      <Layout />
+      <Buying />
+    </>
+  );
+}
+
+export default BuyingPage;
