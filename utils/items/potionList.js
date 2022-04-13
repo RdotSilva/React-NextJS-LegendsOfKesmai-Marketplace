8150b7bbeed3e8f7c70c123a9a9cbc031ceae599
RdotSilva <RyandotSilva@gmail.com>
2022-04-13 20:15:59 +0100

    Add slug for potions

--- utils/items/potionList.js
+++ utils/items/potionList.js
@@ -2,29 +2,36 @@ export const potionList = [
   {
     name: "Strength Potion",
     image: "images/strength-potion.png",
+    slug: "strengthpotion",
   },
   {
     name: "Dexterity Potion",
     image: "images/dexterity-potion.png",
+    slug: "dexteritypotion",
   },
   {
     name: "Intelligence Potion",
     image: "images/intelligence-potion.png",
+    slug: "intelligencepotion",
   },
   {
     name: "Wisdom Potion",
     image: "images/wisdom-potion.png",
+    slug: "wisdompotion",
   },
   {
     name: "Willpower Potion",
     image: "images/willpower-potion.png",
+    slug: "willpowerpotion",
   },
   {
     name: "Constitution Potion",
     image: "images/constitution-potion.png",
+    slug: "constitutionpotion",
   },
   {
     name: "Youth Potion",
     image: "images/youth-potion.png",
+    slug: "youthpotion",
   },
 ];
