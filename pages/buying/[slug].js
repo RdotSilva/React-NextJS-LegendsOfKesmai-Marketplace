8ff53b202bf1a448c4ae5aeb35f86b2a8ff51757
RdotSilva <RyandotSilva@gmail.com>
2022-04-13 20:41:45 +0100

    Add getStaticPaths for potion list

--- pages/buying/[slug].js
+++ pages/buying/[slug].js
@@ -2,13 +2,31 @@ import React from "react";
 import Layout from "../../components/Layout/Layout";
 import { potionList } from "../../utils/items/potionList";
 
-const BuyingDetails = () => {
+const BuyingDetails = ({ potionData }) => {
   return (
     <>
       <Layout />
       <div>Buying Details Page</div>
+      <p>{potionData.name}</p>
     </>
   );
 };
 
 export default BuyingDetails;
+
+/**
+ * Find all slugs and generate static page for each item based on the slug
+ * @returns All of the slugs located inside of potion list
+ */
+export const getStaticPaths = async () => {
+  const paths = potionList.map((potion) => {
+    return {
+      params: { slug: potion.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
