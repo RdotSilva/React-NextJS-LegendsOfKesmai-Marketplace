10f498f54c0f807571c99c12baf030ac94e3a901
RdotSilva <RyandotSilva@gmail.com>
2022-04-14 21:41:27 +0100

    Fix header routes

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="buy"
+                  href="/buy"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Buy
@@ -69,7 +69,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="sell"
+                  href="/sell"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Sell
@@ -77,7 +77,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="trade"
+                  href="/trade"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Trade
