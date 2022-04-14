4b64ab4418c8aea8de743fed982caea174e09ddc
RdotSilva <RyandotSilva@gmail.com>
2022-04-14 21:27:46 +0100

    Update mock item data

--- utils/mockData/itemsForSale.js
+++ utils/mockData/itemsForSale.js
@@ -1,14 +1,22 @@
-export const strengthPotion = [
+export const itemsForSale = [
   {
     user: "User 1",
     value: 20000,
+    slug: "strengthpotion",
   },
   {
     user: "User 2",
     value: 30000,
+    slug: "strengthpotion",
   },
   {
     user: "User 3",
     value: 40000,
+    slug: "strengthpotion",
+  },
+  {
+    user: "User 5",
+    value: 40000,
+    slug: "strengthpotion",
   },
 ];
