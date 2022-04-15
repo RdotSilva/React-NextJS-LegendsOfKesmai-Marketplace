28026e1c89e5eac35a2e64b138397aac059fde84
RdotSilva <RyandotSilva@gmail.com>
2022-04-15 20:38:56 +0100

    Add SellPage component

---
+++ pages/sell.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sell from "../components/Sell/Sell.jsx";
+import Layout from "../components/Layout/Layout.jsx";
+
+function SellPage() {
+  return (
+    <>
+      <Layout />
+      <Sell />
+    </>
+  );
+}
+
+export default SellPage;
