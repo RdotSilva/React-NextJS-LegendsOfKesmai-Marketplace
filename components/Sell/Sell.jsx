65b66d6feafdfd1166d9752d7ef5779f7c94abb8
RdotSilva <RyandotSilva@gmail.com>
2022-04-16 09:28:12 +0100

    Add CategoryList example

--- components/Sell/Sell.jsx
+++ components/Sell/Sell.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Category from "../Category/Category";
+import CategoryList from "../Category/CategoryList";
 import { potionList } from "../../utils/items/potionList";
 
 const Sell = () => {
   return (
     <div>
-      <Category
+      {/* <Category
         categoryName="Potions"
         categoryItems={potionList}
         tradeType="selling"
-      />
+      /> */}
+      <CategoryList />
     </div>
   );
 };
