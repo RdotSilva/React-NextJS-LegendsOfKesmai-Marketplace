32b22a48e2f6bdbff1372163bf4fcd09f6d4cf23
RdotSilva <RyandotSilva@gmail.com>
2022-04-17 19:53:07 +0100

    Add SellingPage component

---
+++ pages/selling/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Selling from "../../components/Sell/Selling.jsx";
+import Layout from "../../components/Layout/Layout.jsx";
+
+function SellingPage() {
+  return (
+    <>
+      <Layout />
+      <Selling />
+    </>
+  );
+}
+
+export default SellingPage;
