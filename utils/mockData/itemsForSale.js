eb50af2cb76c51baed3576fa04999e2e94503101
RdotSilva <RyandotSilva@gmail.com>
2022-04-18 16:49:01 +0100

    Add date for mocked item data

--- utils/mockData/itemsForSale.js
+++ utils/mockData/itemsForSale.js
@@ -3,20 +3,24 @@ export const itemsForSale = [
     user: "User 1",
     value: 20000,
     slug: "strengthpotion",
+    date: 1650296914569,
   },
   {
     user: "User 2",
     value: 30000,
     slug: "strengthpotion",
+    date: 1650296914569,
   },
   {
     user: "User 3",
     value: 40000,
     slug: "strengthpotion",
+    date: 1650296914569,
   },
   {
     user: "User 5",
     value: 40000,
     slug: "strengthpotion",
+    date: 1650296914569,
   },
 ];
