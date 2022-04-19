b966dea52d6c86226e9fc927913b3dd765e21f53
RdotSilva <RyandotSilva@gmail.com>
2022-04-19 18:17:58 +0100

    Add h1 for item name

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Table = ({ items }) => {
   return (
     <>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="relative overflow-x-auto shadow-md ">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
