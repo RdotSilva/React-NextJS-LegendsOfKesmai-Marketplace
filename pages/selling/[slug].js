f52b8ff9d5620a50bc8fb9e7255a9c05508bb429
RdotSilva <RyandotSilva@gmail.com>
2022-04-20 21:05:15 +0100

    Move title into Table component

--- pages/selling/[slug].js
+++ pages/selling/[slug].js
@@ -8,13 +8,8 @@ const SellingDetails = ({ potionData, forSale }) => {
   return (
     <>
       <Layout />
-      <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-          {potionData.name}
-        </span>
-      </h1>
       <div>
-        <Table items={itemsForSale} />
+        <Table items={itemsForSale} itemName={potionData.name} />
       </div>
     </>
   );
