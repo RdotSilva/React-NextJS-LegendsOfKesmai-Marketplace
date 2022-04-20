35c13b56627a9b5979b3994ff9acde95c60000a5
RdotSilva <RyandotSilva@gmail.com>
2022-04-20 21:05:48 +0100

    Update flex layout to handle column

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import moment from "moment";
 
-const Table = ({ items }) => {
+const Table = ({ items, itemName }) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center">
+      <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+          {itemName}
+        </span>
+      </h1>
       <div className="relative overflow-x-auto shadow-md">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
