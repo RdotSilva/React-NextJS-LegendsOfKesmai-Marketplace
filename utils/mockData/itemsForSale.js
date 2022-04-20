6019edd4f38b45e9556fe655f17198aefd39d770
RdotSilva <RyandotSilva@gmail.com>
2022-04-20 21:09:49 +0100

    Update mock data for dex pot

--- utils/mockData/itemsForSale.js
+++ utils/mockData/itemsForSale.js
@@ -23,4 +23,28 @@ export const itemsForSale = [
     slug: "strengthpotion",
     date: 1650296914569,
   },
+  {
+    user: "User 6",
+    value: 200,
+    slug: "dexteritypotion",
+    date: 1650296914569,
+  },
+  {
+    user: "User 7",
+    value: 3000,
+    slug: "dexteritypotion",
+    date: 1650296914569,
+  },
+  {
+    user: "User 8",
+    value: 22000,
+    slug: "dexteritypotion",
+    date: 1650296914569,
+  },
+  {
+    user: "User 9",
+    value: 45000,
+    slug: "dexteritypotion",
+    date: 1650296914569,
+  },
 ];
