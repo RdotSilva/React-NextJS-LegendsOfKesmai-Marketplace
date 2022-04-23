29c34a624a45c0bd9b815462a3b7641bb0e1af9c
RdotSilva <RyandotSilva@gmail.com>
2022-04-23 13:49:15 +0100

    Move db test functions

--- utils/firebase/testDB.js
+++ utils/firebase/testDB.js
@@ -1,12 +1,37 @@
-import { db } from "./initFirebase";
+import { db } from "./initFirebase.js";
+import { collection, addDoc, getDocs, doc, setDoc } from "firebase/firestore";
 
-var leadsRef = database.ref("potions");
-leadsRef.on("value", function (snapshot) {
-  snapshot.forEach(function (childSnapshot) {
-    var childData = childSnapshot.val();
+const dbInstance = collection(db, "potions");
+
+// Create the doc ref used for setting a document and a custom ID
+const docRef = doc(db, "potions", "00225");
+
+// Potion payload for testing
+const potion = {
+  name: "Strength Potion",
+  image: "images/item-00225.png",
+  slug: "strengthpotion",
+  id: "00225",
+};
+
+// Set the payload in the db with custom ID
+const savePotions = () => {
+  setDoc(docRef, {
+    potion,
   });
-});
+};
 
-leadsRef.on("child_added", function (snapshot) {
-  console.log(snapshot);
-});
+// savePotions();
+
+// Fetch the data from db
+const getPotions = () => {
+  getDocs(dbInstance).then((data) => {
+    console.log(
+      data.docs.map((item) => {
+        return { ...item.data(), id: item.id };
+      })
+    );
+  });
+};
+
+getPotions();
