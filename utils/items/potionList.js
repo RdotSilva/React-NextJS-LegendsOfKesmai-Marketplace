607a0304020d4d43fe950c8c632d2f99f46f5119
RdotSilva <RyandotSilva@gmail.com>
2022-04-23 13:19:56 +0100

    Update potion list

--- utils/items/potionList.js
+++ utils/items/potionList.js
@@ -1,37 +1,56 @@
 export const potionList = [
   {
     name: "Strength Potion",
-    image: "images/strength-potion.png",
+    image: "images/item-00225.png",
     slug: "strengthpotion",
+    id: "00225",
   },
   {
     name: "Dexterity Potion",
-    image: "images/dexterity-potion.png",
+    image: "images/item-00222.pngg",
     slug: "dexteritypotion",
+    id: "00222",
   },
   {
     name: "Intelligence Potion",
-    image: "images/intelligence-potion.png",
+    image: "images/item-00216.png",
     slug: "intelligencepotion",
+    id: "00216",
   },
   {
     name: "Wisdom Potion",
-    image: "images/wisdom-potion.png",
+    image: "images/item-00274.png",
     slug: "wisdompotion",
+    id: "00274",
   },
   {
     name: "Willpower Potion",
-    image: "images/willpower-potion.png",
+    image: "images/item-00289.png",
     slug: "willpowerpotion",
+    id: "00289",
   },
   {
     name: "Constitution Potion",
-    image: "images/constitution-potion.png",
+    image: "images/item-00303.png",
     slug: "constitutionpotion",
+    id: "00303",
   },
   {
     name: "Youth Potion",
-    image: "images/youth-potion.png",
+    image: "images/item-00227.png",
     slug: "youthpotion",
+    id: "00227",
+  },
+  {
+    name: "Mana Replenishment Potion",
+    image: "images/item-00006.png",
+    slug: "manareplenishmentpotion",
+    id: "00006",
+  },
+  {
+    name: "Permanent Mana Potion",
+    image: "images/item-00316.png",
+    slug: "permanentmanapotion",
+    id: "00227",
   },
 ];
