2e4a3b56ceef202968926aa7ccea58918971310e
RdotSilva <RyandotSilva@gmail.com>
2022-04-25 20:19:37 +0100

    Add getStaticProps with database call

--- pages/sell.js
+++ pages/sell.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Sell from "../components/Sell/Sell.jsx";
 import Layout from "../components/Layout/Layout.jsx";
+import { db } from "../utils/firebase/initFirebase.js";
+import { fetchDocuments } from "../utils/firebase/operations.js";
+import { collection } from "firebase/firestore";
 
-function SellPage() {
+const potionsCollectionRef = collection(db, "potions");
+
+function SellPage({ potionList }) {
   return (
     <>
       <Layout />
-      <Sell />
+      <Sell potionList={potionList} />
     </>
   );
 }
 
 export default SellPage;
+
+/**
+ * Query the database to fetch all potion data and send that as props
+ * @param {*} context
+ * @returns
+ */
+export const getStaticProps = async (context) => {
+  const potionData = await fetchDocuments(potionsCollectionRef);
+  return {
+    props: { potionList: potionData },
+  };
+};
