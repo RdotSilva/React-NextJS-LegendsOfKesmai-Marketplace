e7cd9090bdc9d25d0d70ab402f72f0f18779aa8b
RdotSilva <RyandotSilva@gmail.com>
2022-04-29 21:10:50 +0100

    Add Buy component

---
+++ components/Buy/Buy.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CategoryList from "../Category/CategoryList";
+
+/**
+ * This is the main component that is rendered on the /buy page
+ * It should show a row of clickable item categories and each category should have a dropdown for items
+ */
+const Buy = ({ potionList }) => {
+  return (
+    <div>
+      <CategoryList potionList={potionList} />
+    </div>
+  );
+};
+
+export default Buy;
