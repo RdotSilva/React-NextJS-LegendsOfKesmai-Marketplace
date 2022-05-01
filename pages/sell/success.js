61c1401fab5fe120782d1c70a2108347d710db96
RdotSilva <RyandotSilva@gmail.com>
2022-05-01 12:06:33 +0100

    Add page for sell success

---
+++ pages/sell/success.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout.jsx";
+
+/**
+ * Render the /sell/success
+ */
+function SellSuccessPage({ potionList }) {
+  return (
+    <>
+      <Layout />
+      <p>Success!</p>
+    </>
+  );
+}
+
+export default SellSuccessPage;
