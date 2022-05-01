204eed1d1e6461231bf706e457b2e0b009f6e3c0
RdotSilva <RyandotSilva@gmail.com>
2022-05-01 13:11:15 +0100

    Add SellSuccessPage

--- pages/sell/success.js
+++ pages/sell/success.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout.jsx";
+import { useRouter } from "next/router";
+import SellSuccess from "../../components/Sell/SellSuccess.jsx";
 
 /**
- * Render the /sell/success
+ * Render the /sell/success page
  */
-function SellSuccessPage({ potionList }) {
+function SellSuccessPage() {
+  const router = useRouter();
+
+  // Fetch item data from the router query -> going to send this as props to success component
+  const { query: successfulItemListing } = router;
+
   return (
     <>
       <Layout />
-      <p>Success!</p>
+      <SellSuccess itemListing={successfulItemListing} />
     </>
   );
 }
