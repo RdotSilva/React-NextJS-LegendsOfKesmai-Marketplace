5a2798f3111f0a0dff3e8fa77483bf562537164d
RdotSilva <RyandotSilva@gmail.com>
2022-05-01 13:26:14 +0100

    Add MyListingsPage

---
+++ pages/my-listings/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MyListings from "../../components/MyListings/MyListings";
+
+const MyListingsPage = () => {
+  return <MyListings />;
+};
+
+export default MyListingsPage;
