663393e7cd3aed0c2c10cbcaf0c4bbe1e7b1b01e
RdotSilva <RyandotSilva@gmail.com>
2022-05-03 21:30:16 +0100

    Add onDeleteListing function

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
+import { deleteDocument } from "../../utils/firebase/operations";
+import { doc } from "firebase/firestore";
+import { db } from "../../utils/firebase/initFirebase";
 
 const Table = ({ items, itemName, isMyListingTable }) => {
+  /**
+   * Delete a listing from the database
+   * This should only be allowed for the user who owns the listing
+   * @param {String} docId Document reference ID for the collection to delete
+   */
+  const onDeleteListing = async (docId) => {
+    const listingRef = doc(db, "selling", docId);
+    deleteDocument(listingRef);
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
@@ -56,6 +69,7 @@ const Table = ({ items, itemName, isMyListingTable }) => {
                 {isMyListingTable && (
                   <td className="px-6 py-4">
                     <button
+                      onClick={() => onDeleteListing(item.docId)}
                       type="button"
                       className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
                     >
