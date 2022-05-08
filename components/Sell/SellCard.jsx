c13ead05e178d25ee6e5ec27904336be9f5adc01
RdotSilva <RyandotSilva@gmail.com>
2022-05-08 19:58:12 +0100

    Update styles for card

--- components/Sell/SellCard.jsx
+++ components/Sell/SellCard.jsx
@@ -63,7 +63,7 @@ const SellCard = ({ potionData }) => {
   };
 
   const counter = (
-    <div className="custom-number-input h-10 w-32">
+    <div className="mb-2 custom-number-input h-10 w-32">
       <label
         htmlFor="custom-input-number"
         className="block text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -130,7 +130,7 @@ const SellCard = ({ potionData }) => {
           </div>
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-one dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={(e) => submitHandler(e)}
           >
             Sell
