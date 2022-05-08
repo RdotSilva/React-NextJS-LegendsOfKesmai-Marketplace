a8d1cfd83b7e9824350e36ff42f6eecd239eacce
RdotSilva <RyandotSilva@gmail.com>
2022-05-08 18:29:56 +0100

    Clean up styles and center card

--- components/Sell/SellCard.jsx
+++ components/Sell/SellCard.jsx
@@ -66,7 +66,7 @@ const SellCard = ({ potionData }) => {
     <div className="custom-number-input h-10 w-32">
       <label
         htmlFor="custom-input-number"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        className="block text-sm font-medium text-gray-900 dark:text-gray-300"
       >
         Quantity
       </label>
@@ -97,7 +97,7 @@ const SellCard = ({ potionData }) => {
   );
 
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form className="space-y-6" action="#">
           <h5 className="text-xl font-medium text-gray-900 dark:text-white">
