125451c734fb0b276bd4cf29dec00cff0253bba7
RdotSilva <RyandotSilva@gmail.com>
2022-05-09 22:05:08 +0100

    Add armorCollectionRef for db

--- utils/firebase/collectionRefs.js
+++ utils/firebase/collectionRefs.js
@@ -4,5 +4,8 @@ import { collection } from "firebase/firestore";
 // Potion data
 export const potionsCollectionRef = collection(db, "potions");
 
+// Armor data
+export const armorCollectionRef = collection(db, "armor");
+
 // Listings for items that are being sold
 export const sellingCollectionRef = collection(db, "selling");
