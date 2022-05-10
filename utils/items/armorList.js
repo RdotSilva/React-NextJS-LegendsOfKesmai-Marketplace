27125147d64f899759921888a076d46d457eed96
RdotSilva <RyandotSilva@gmail.com>
2022-05-10 19:08:23 +0100

    Add armor list used for seeding

---
+++ utils/items/armorList.js
@@ -0,0 +1,44 @@
+export const armorList = [
+  {
+    name: "Ice Scales",
+    image: "item-00219.png",
+    slug: "icescales",
+    id: "00219",
+  },
+  {
+    name: "Drake Scales",
+    image: "item-00220.png",
+    slug: "drakescales",
+    id: "00220",
+  },
+  {
+    name: "Star Robe",
+    image: "item-00238.png",
+    slug: "starrobe",
+    id: "00238",
+  },
+  {
+    name: "Crocodile Boots",
+    image: "item-001224.png",
+    slug: "crocodileboots",
+    id: "00274",
+  },
+  {
+    name: "Feather Fall Boots",
+    image: "item-00131.png",
+    slug: "featherfallboots",
+    id: "00131",
+  },
+  {
+    name: "Steel Gauntlets",
+    image: "item-00069.png",
+    slug: "steelgauntlets",
+    id: "00069",
+  },
+  {
+    name: "Griffin Skull",
+    image: "item-00035.png",
+    slug: "griffinskull",
+    id: "00035",
+  },
+];
