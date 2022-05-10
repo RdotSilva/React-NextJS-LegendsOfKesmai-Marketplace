acc5249a2117b1a995d005239f358d6ee4b54aba
RdotSilva <RyandotSilva@gmail.com>
2022-05-10 19:13:34 +0100

    Fix TODO for fetching armor data for props

--- pages/buy/index.js
+++ pages/buy/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import Buy from "../../components/Buy/Buy.jsx";
 import Layout from "../../components/Layout/Layout.jsx";
 import { fetchDocuments } from "../../utils/firebase/operations.js";
-import { potionsCollectionRef } from "../../utils/firebase/collectionRefs.js";
+import {
+  armorCollectionRef,
+  potionsCollectionRef,
+} from "../../utils/firebase/collectionRefs.js";
 
 /**
  * Render the /buy page
@@ -25,16 +28,16 @@ export default BuyPage;
  */
 export const getStaticProps = async (context) => {
   const potionData = await fetchDocuments(potionsCollectionRef);
+  const armorData = await fetchDocuments(armorCollectionRef);
   // TODO: Fetch these from database then they are seeded
-  // const armorData = await fetchDocuments(armorCollectionRef);
   // const jewelryData = await fetchDocuments(jewelryCollectionRef);
   // const weaponsData = await fetchDocuments(weaponsCollectionRef);
   // const miscData = await fetchDocuments(miscCollectionRef);
 
   const categories = {
     Potions: potionData,
+    Armor: armorData,
     // TODO: Pass the new data for each category into the components
-    // Armor: armorData,
     // Jewelry: jewelryData,
     // Weapons: weaponsData,
     // Misc: miscData,
