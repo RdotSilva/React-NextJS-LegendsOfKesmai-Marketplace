73edb317d95d14f55478f1666b70a8ef2a8aaf03
RdotSilva <RyandotSilva@gmail.com>
2022-05-17 20:20:30 +0100

    Convert navbar items to Links

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { useAuth } from "../../context/authUserContext";
 
 const NavBar = () => {
@@ -8,16 +9,18 @@ const NavBar = () => {
     <div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="/" className="flex items-center">
-            <img
-              src="\images\kesmai-dragon-red.png"
-              className="mr-3 h-6 sm:h-9"
-              alt="Reg Dragon"
-            ></img>
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              LOK Marketplace
-            </span>
-          </a>
+          <Link href="/">
+            <a className="flex items-center">
+              <img
+                src="\images\kesmai-dragon-red.png"
+                className="mr-3 h-6 sm:h-9"
+                alt="Reg Dragon"
+              ></img>
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                LOK Marketplace
+              </span>
+            </a>
+          </Link>
           <button
             data-collapse-toggle="mobile-menu"
             type="button"
@@ -55,61 +58,57 @@ const NavBar = () => {
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               {authUser && (
                 <li>
-                  <a
-                    href="/sell"
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Sell
-                  </a>
+                  <Link href="/sell">
+                    <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                      Sell
+                    </a>
+                  </Link>
                 </li>
               )}
               <li>
-                <a
-                  href="/buy"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Buy
-                </a>
+                <Link href="/buy">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    Buy
+                  </a>
+                </Link>
               </li>
               {!authUser && (
                 <li>
-                  <a
-                    href="/login"
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Login
-                  </a>
+                  <Link href="/login">
+                    <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                      Login
+                    </a>
+                  </Link>
                 </li>
               )}
               {!authUser && (
                 <li>
-                  <a
-                    href="/signup"
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Sign Up
-                  </a>
+                  <Link href="/signup">
+                    <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                      Sign Up
+                    </a>
+                  </Link>
                 </li>
               )}
               {authUser && (
                 <li>
-                  <a
-                    href="/my-listings"
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    My Listings
-                  </a>
+                  <Link href="/my-listings">
+                    <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                      My Listings
+                    </a>
+                  </Link>
                 </li>
               )}
               {authUser && (
                 <li>
-                  <a
-                    href="#"
-                    onClick={signOutUser}
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Sign Out
-                  </a>
+                  <Link href="#">
+                    <a
+                      onClick={signOutUser}
+                      className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
+                      Sign Out
+                    </a>
+                  </Link>
                 </li>
               )}
             </ul>
