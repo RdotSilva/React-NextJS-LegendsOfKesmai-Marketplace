2f728d0017bd0290724136f2b7f3b5478f5a2046
RdotSilva <RyandotSilva@gmail.com>
2022-05-21 11:00:02 +0100

    Fix extra nav bar

--- components/MyListings/NoListings.jsx
+++ components/MyListings/NoListings.jsx
@@ -7,7 +7,6 @@ import Layout from "../Layout/Layout";
 const NoListings = () => {
   return (
     <>
-      <Layout />
       <div className="flex flex-col items-center">
         <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
