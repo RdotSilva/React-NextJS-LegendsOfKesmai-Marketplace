5740dc61aa4d591aa9af003a3d68c6ec33a5f5ae
RdotSilva <RyandotSilva@gmail.com>
2022-05-23 20:57:04 +0100

    FIx redirect when user logs in

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -22,7 +22,7 @@ const SignUpPage = () => {
       createUser(email, passwordOne)
         .then((authUser) => {
           console.log("Success. The user is created in Firebase");
-          router.push("/logged_in");
+          router.push("/my-listings");
         })
         .catch((error) => {
           // An error occurred. Set error message to be displayed to user
